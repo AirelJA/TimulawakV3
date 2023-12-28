e48a95aed8b93fc33ad9e068cfd9a19d50714b9b
AirelJA <102723962+AirelJA@users.noreply.github.com>
2023-12-28 15:47:13 +0700

    add jump physics and fixed prefabs

--- Assets/Script/playerMovement.cs
+++ Assets/Script/playerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private bool onGround = true;
+    public bool gameOver = false;
     private float horizontalMovement;
     private GameObject focalPoint;
     public float speed = 5.0f;
@@ -13,6 +15,8 @@ public class playerMovement : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
+
+        
     }
 
     // Update is called once per frame
@@ -20,6 +24,25 @@ public class playerMovement : MonoBehaviour
     {
         horizontalMovement = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontalMovement);
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            playerRb.AddForce(Vector3.up * 4, ForceMode.Impulse);
+            onGround = false;
+        }
         //playerRb.AddForce(focalPoint.transform.forward * speed * forwardMovement);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+
+        else if (collision.gameObject.CompareTag("deathTrigger"))
+        {
+            gameOver = true;
+            onGround = false;
+        }
     }
 }
